e5830189ef3715806e9b2de69c461417c8b7c9d2
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-01 10:27:44 +0530

    add some data

---
+++ script.js
@@ -0,0 +1,28 @@
+// Fire Signs (Aries, Leo, Sagittarius), Earth Signs (Taurus, Virgo, Capricorn), Air Signs (Gemini, Libra, Aquarius), and Water Signs (Cancer, Scorpio, Pisces).
+const data = {
+  horoscope: [
+    "Aries",
+    "Leo",
+    "Sagittarius",
+    "Taurus",
+    "Virgo",
+    "Capricorn",
+    "Gemini",
+    "Libra",
+    "Aquarius",
+    "Cancer",
+    "Scorpio",
+    "Pisces",
+  ],
+  todayFortune: [
+    "Very Lucky",
+    "Slightly Lucky",
+    "Unlucky",
+    "Very Unlucky",
+  ],
+  advice: {
+    message1: ["get", "eat", "make", "give"],
+    message2: ["tea", "noodles", "paper", "water", "rice"],
+  },
+};
+
